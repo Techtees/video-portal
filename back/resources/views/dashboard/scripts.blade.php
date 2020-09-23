4c9383c6250d79aeae3050c56b98e72bc56a382d
Adesanoye Samson <adesanoye.samson@yahoo.com>
2020-09-23 20:05:14 +0100

    successfully port login page to laravel blade

--- back/resources/views/dashboard/scripts.blade.php
+++ back/resources/views/dashboard/scripts.blade.php
@@ -1,12 +1,12 @@
 <!-- Bootstrap core JavaScript-->
-<script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
-<script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('js/jquery.min.js') }}"></script>
+<script src="{{ asset('bootstrap/js/bootstrap.min.js') }}"></script>
 
 <!-- Core plugin JavaScript-->
 <script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
 
 <!-- Custom scripts for all pages-->
-<script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
+<script src="{{ asset('js/custom.js') }}"></script>
 
 <!-- Page level plugins -->
 <script src="{{ asset('vendor/chart.js/Chart.min.js') }}"></script>
--- back/routes/web.php
+++ back/routes/web.php
@@ -16,7 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get(
     '/',
     function () {
-        return redirect( route('login') );
+        // return redirect( route('login') );
+        return view('layouts.app');
     }
 );
 
